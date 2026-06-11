05e867543085d45cafb15c70d32e746bb634766a
MacBook <macbook@MacBook-Air-Demi.local>
2026-06-11 13:50:55 +0300

    add root route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Moodle GIFT API is running")
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
